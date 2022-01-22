07578eecbd38eab99a7ad252be41f2a8056a263d
prayz212 <49938194+prayz212@users.noreply.github.com>
2022-01-22 22:23:01 +0700

    UI/ux enhancement v1 (#91)
    * fix overflow text
    
    * try catch exception handler
    
    * Update comment.js

--- app/Http/Controllers/Client/AccountController.php
+++ app/Http/Controllers/Client/AccountController.php
@@ -14,77 +14,119 @@ use Illuminate\Support\Facades\Hash;
 class AccountController extends Controller
 {
     public function index() {
-        $customer = Auth::user();
+        try {
+            $customer = Auth::user();
 
-        return view('client.info-page')
-            ->with('customer', $customer);
+            return view('client.info-page')
+                ->with('customer', $customer);
+        }
+        catch(\Exception $error){
+            return view('error')
+                ->with('errorMessages', $error->getMessage())
+                ->with('returnUrl', url()->previous());
+        }
     }
 
     public function updateInfo(UpdateInfoRequest $request) {
-        $customer = Customer::find($request->id);
+        try {
+            $customer = Customer::find($request->id);
 
-        if ($customer->email != $request->email) {
-            $isExistEmail = Customer::where('email', $request->email)
-                ->exists();
-        
-            if ($isExistEmail) {
-                return redirect()->back()->withInput()->with('info-noti-error', 'Email đã tồn tại');
+            if ($customer->email != $request->email) {
+                $isExistEmail = Customer::where('email', $request->email)
+                    ->exists();
+            
+                if ($isExistEmail) {
+                    return redirect()->back()->withInput()->with('info-noti-error', 'Email đã tồn tại');
+                }
             }
+
+            $customer->fullname = $request->fullname;
+            $customer->phone = $request->phone;
+            $customer->email = $request->email;
+            $customer->address = $request->address;
+            $customer->save();
+
+            return redirect()->route('infopage')->with('info-noti-success', 'Cập nhật thành công');
         }
-
-        $customer->fullname = $request->fullname;
-        $customer->phone = $request->phone;
-        $customer->email = $request->email;
-        $customer->address = $request->address;
-        $customer->save();
-
-        return redirect()->route('infopage')->with('info-noti-success', 'Cập nhật thành công');
+        catch(\Exception $error){
+            return view('error')
+                ->with('errorMessages', $error->getMessage())
+                ->with('returnUrl', url()->previous());
+        }
     }
 
     public function billDetail($id) {
-        $bill = Bill::with([
-            'rating',
-            'bill_detail',
-            'bill_detail.item',
-            'bill_detail.item.main_pic',
-            'bill_detail.item.category',
-            'voucher',
-            'card'
-        ])
-        ->where('customer_id', Auth::user()->id)
-        ->find($id);
+        try {
+            $bill = Bill::with([
+                'rating',
+                'bill_detail',
+                'bill_detail.item',
+                'bill_detail.item.main_pic',
+                'bill_detail.item.category',
+                'voucher',
+                'card'
+            ])
+            ->where('customer_id', Auth::user()->id)
+            ->find($id);
 
-        return view('client.bill-detail')
-            ->with('bill', $bill);
+            return view('client.bill-detail')
+                ->with('bill', $bill);
+        }
+        catch(\Exception $error){
+            return view('error')
+                ->with('errorMessages', $error->getMessage())
+                ->with('returnUrl', url()->previous());
+        }
     }
 
     public function bills() {
-        $bills = Bill::where('customer_id', Auth::user()->id)
-            ->get();
-        return view('client.bill-page')
-            ->with('bills', $bills);
+        try {
+            $bills = Bill::where('customer_id', Auth::user()->id)
+                ->get();
+            return view('client.bill-page')
+                ->with('bills', $bills);
+        }
+        catch(\Exception $error){
+            return view('error')
+                ->with('errorMessages', $error->getMessage())
+                ->with('returnUrl', url()->previous());
+        }
     }
 
     public function resetPassword() {
-        $customer = Auth::user();
+        try {
+            $customer = Auth::user();
 
-        return view('client.reset-password')
-            ->with('customer', $customer);
+            return view('client.reset-password')
+                ->with('customer', $customer);
+        }
+        catch(\Exception $error){
+            return view('error')
+                ->with('errorMessages', $error->getMessage())
+                ->with('returnUrl', url()->previous());
+        }
     }
 
     public function requestNewPassword(NewPasswordRequest $request) {
-        $customer = Customer::where('username', $request->username)
-                        ->first();
+        try {
+            $customer = Customer::where('username', $request->username)
+                            ->first();
 
-        if (!password_verify($request->oldPassword, $customer->password)) {
-            return redirect()->back()
-                    ->with('request-newpassword-err', 'Mật khẩu cũ không đúng');
+            if (!password_verify($request->oldPassword, $customer->password)) {
+                return redirect()->back()
+                        ->with('request-newpassword-err', 'Mật khẩu cũ không đúng');
+            }
+
+            $customer->password = Hash::make($request->newPassword);
+            $customer->save();
+
+            return redirect()->route('infopage')
+                    ->with('info-noti-success', 'Cập nhật mật khẩu thành công');
         }
-
-        $customer->password = Hash::make($request->newPassword);
-        $customer->save();
-
-        return redirect()->route('infopage')
-                ->with('info-noti-success', 'Cập nhật mật khẩu thành công');
+        catch(\Exception $error){
+            return view('error')
+                ->with('errorMessages', $error->getMessage())
+                ->with('returnUrl', url()->previous());
+        }
     }
 }
--- app/Http/Controllers/Client/CartController.php
+++ app/Http/Controllers/Client/CartController.php
@@ -23,192 +23,238 @@ class CartController extends Controller
     private $BILL_STATUS = ['IN_PROCESS' => 'Đang xử lý', 'IN_DELIVERY' => 'Đang giao hàng', 'DELIVERED' => 'Đã giao', 'CANCELED' => 'Đã huỷ'];
 
     public function addToCart(Request $request) {
-        $product = Product::with(['main_pic', 'category'])->find($request->id);
-        $oldCart = $request->session()->get('cart', null);
-        $cart = new Cart($oldCart);
-        $cart->add($product, $request->quantity);
-        $request->session()->put('cart', $cart);
+        try {
+            $product = Product::with(['main_pic', 'category'])->find($request->id);
+            $oldCart = $request->session()->get('cart', null);
+            $cart = new Cart($oldCart);
+            $cart->add($product, $request->quantity);
+            $request->session()->put('cart', $cart);
 
-        return response()->json([
-            'status' => 200
-        ]);
+            return response()->json([
+                'status' => 200
+            ]);
+        }
+        catch(\Exception $error){
+            return response()->json(['status' => 400, 'errorMessages' => $error->getMessage()], 400);
+        }
     }
 
     public function updateToCart(Request $request) {
-        $oldCart = $request->session()->get('cart', null);
-        $cart = new Cart($oldCart);
-        $cart->updateQuantity($request->id, $request->quantity);
-        $request->session()->put('cart', $cart);
+        try {
+            $oldCart = $request->session()->get('cart', null);
+            $cart = new Cart($oldCart);
+            $cart->updateQuantity($request->id, $request->quantity);
+            $request->session()->put('cart', $cart);
 
-        return response()->json([
-            'status' => 200
-        ]);
+            return response()->json([
+                'status' => 200
+            ]);
+        }
+        catch(\Exception $error){
+            return response()->json(['status' => 400, 'errorMessages' => $error->getMessage()], 400);
+        }
     }
 
     public function deleteFormCart(Request $request) {
-        $oldCart = $request->session()->get('cart', null);
-        $cart = new Cart($oldCart);
-        $cart->deleteItem($request->id);
-        $request->session()->put('cart', $cart);
+        try {
+            $oldCart = $request->session()->get('cart', null);
+            $cart = new Cart($oldCart);
+            $cart->deleteItem($request->id);
+            $request->session()->put('cart', $cart);
 
-        return response()->json([
-            'status' => 200
-        ]);
+            return response()->json([
+                'status' => 200
+            ]);
+        }
+        catch(\Exception $error){
+            return response()->json(['status' => 400, 'errorMessages' => $error->getMessage()], 400);
+        }
     }
 
     public function index(Request $request) {
-        $carts = $request->session()->get('cart', null);
+        try {
+            $carts = $request->session()->get('cart', null);
 
-        return view('client.shopping-cart')
-            ->with('carts', $carts);
+            return view('client.shopping-cart')
+                ->with('carts', $carts);
+        }
+        catch(\Exception $error){
+            return view('error')
+                ->with('errorMessages', $error->getMessage())
+                ->with('returnUrl', url()->previous());
+        }
     }
 
     public function checkVoucher(Request $request) {
-        $voucherCode = $request->voucher;
-        $isExist = Voucher::where('code', $voucherCode)
-            ->where('start_at', '<=', Carbon::now()->toDateString())
-            ->where('end_at', '>=', Carbon::now()->toDateString())
-            ->where('is_used', false)
-            ->where('isDelete', false)
-            ->first();
-
-        return $isExist == NULL 
-            ? response()->json(['status' => 404])
-            : response()->json(['status' => 200, 'voucher' => $isExist]);
-    }
-
-    public function payment(Request $request) {
-        if ($request->session()->has('cart') && count($request->session()->get('cart')->items)) {
-            $customer = Customer::find(Auth::id());
-            $totalPrice = 0;
-            $totalDiscount = 0;
-            $shippingCost = $this->SHIPPING_COST;
-            $carts = Session::get('cart')->items;
-
-            foreach($carts as $c) {
-                $qty = $c['qty'];
-                $price = $c['item']->price;
-                $discount = $c['item']->discount;
-
-                if ($discount == NULL) {
-                    $totalPrice += ($qty * $price);
-                }
-                else if ($discount != NULL) {
-                    $finalPrice = $price - ($price * (float)$discount/100);
-                    $totalDiscount += ($price - $finalPrice) * $qty;
-                    $totalPrice += ($qty * $finalPrice);
-                }
-            }
-
-            return view('client.payment')
-                ->with('customer', $customer)
-                ->with('totalPrice', $totalPrice)
-                ->with('totalDiscount', $totalDiscount)
-                ->with('shippingCost', $shippingCost);
-        }
-        else {
-            return redirect()->route('shoppingCart');
-        }
-    }
-
-    public function paymentProcess(PaymentRequest $request) {
-        $checkResult = $this->checkQuantity();
-        if ($checkResult['status'] == 0) {
-            return redirect()
-                ->route('shoppingCart')
-                ->with(
-                    'cart-error', 
-                    $checkResult['availableQty'] == '0' 
-                        ? $checkResult['product'] .  ' hiện tại đã hết hàng'
-                        : $checkResult['product'] .  ' chỉ còn ' . $checkResult['availableQty'] . 'kg');
-        }
-
-        if ($request->paymentType == 'CreditCard') {
-            $card = Card::where('number', $request->cardNumber)
-                        ->where('cvc', $request->secretNumber)
-                        ->first();
-            if ($card == null) {
-                return redirect()
-                    ->back()
-                    ->withInput()
-                    ->with('payment-error', 'Thẻ tín dụng không hợp lệ, vui lòng kiểm tra lại.');
-            }
-        }
-
-        $carts = Session::get('cart')->items;
-        $totalAmount = $this->paymentCalculation($carts);
-
-        if ($request->voucher != NULL) {
-            $voucher = Voucher::where('code', $request->voucher)
+        try {
+            $voucherCode = $request->voucher;
+            $isExist = Voucher::where('code', $voucherCode)
                 ->where('start_at', '<=', Carbon::now()->toDateString())
                 ->where('end_at', '>=', Carbon::now()->toDateString())
                 ->where('is_used', false)
                 ->where('isDelete', false)
                 ->first();
-            if ($voucher != NULL) {
-                $totalAmount['totalPay'] -= $voucher->discount;   
-            } else {
-                return redirect()
-                    ->back()
-                    ->withInput()
-                    ->with('payment-voucher-error', 'Voucher không hợp lệ hoặc đã hết hạn');
+
+            return $isExist == NULL 
+                ? response()->json(['status' => 404])
+                : response()->json(['status' => 200, 'voucher' => $isExist]);
+        }
+        catch(\Exception $error){
+            return response()->json(['status' => 400, 'errorMessages' => $error->getMessage()], 400);
+        }
+    }
+
+    public function payment(Request $request) {
+        try {
+            if ($request->session()->has('cart') && count($request->session()->get('cart')->items)) {
+                $customer = Customer::find(Auth::id());
+                $totalPrice = 0;
+                $totalDiscount = 0;
+                $shippingCost = $this->SHIPPING_COST;
+                $carts = Session::get('cart')->items;
+
+                foreach($carts as $c) {
+                    $qty = $c['qty'];
+                    $price = $c['item']->price;
+                    $discount = $c['item']->discount;
+
+                    if ($discount == NULL) {
+                        $totalPrice += ($qty * $price);
+                    }
+                    else if ($discount != NULL) {
+                        $finalPrice = $price - ($price * (float)$discount/100);
+                        $totalDiscount += ($price - $finalPrice) * $qty;
+                        $totalPrice += ($qty * $finalPrice);
+                    }
+                }
+
+                return view('client.payment')
+                    ->with('customer', $customer)
+                    ->with('totalPrice', $totalPrice)
+                    ->with('totalDiscount', $totalDiscount)
+                    ->with('shippingCost', $shippingCost);
+            }
+            else {
+                return redirect()->route('shoppingCart');
             }
         }
-
-        $bill = Bill::create([
-            'fullname' => $request->fullname,
-            'email' => $request->email,
-            'address' => $request->address,
-            'phone' => $request->phone,
-            'notes' => $request->notes ?? '',
-            'status' => $this->BILL_STATUS['IN_PROCESS'],
-            'totalPrice' => $totalAmount['totalPrice'],
-            'totalDiscount' => $totalAmount['totalDiscount'],
-            'shippingCost' => $totalAmount['totalShippingCost'],
-            'totalPay' => $totalAmount['totalPay'],
-            'voucher_id' => $voucher->id ?? null,
-            'method' => $request->paymentType,
-            'card_id' => $card->id ?? null,
-            'customer_id' => Auth::user()->id,
-        ]);
-
-        if (isset($voucher)) {
-            $voucher->is_used = true;
-            $voucher->save();
+        catch(\Exception $error){
+            return view('error')
+                ->with('errorMessages', $error->getMessage())
+                ->with('returnUrl', url()->previous());
         }
+    }
 
-        foreach($carts as $key => $value)
-        {
-            $bill_detail = BillDetail::create([
-                'bill_id' => $bill->id,
-                'product_id' => $key,
-                'quantity' => $value['qty'],
-                'unit_price' => $value['item']->price,
+    public function paymentProcess(PaymentRequest $request) {
+        try {
+            $checkResult = $this->checkQuantity();
+            if ($checkResult['status'] == 0) {
+                return redirect()
+                    ->route('shoppingCart')
+                    ->with(
+                        'cart-error', 
+                        $checkResult['availableQty'] == '0' 
+                            ? $checkResult['product'] .  ' hiện tại đã hết hàng'
+                            : $checkResult['product'] .  ' chỉ còn ' . $checkResult['availableQty'] . 'kg');
+            }
+
+            if ($request->paymentType == 'CreditCard') {
+                $card = Card::where('number', $request->cardNumber)
+                            ->where('cvc', $request->secretNumber)
+                            ->first();
+                if ($card == null) {
+                    return redirect()
+                        ->back()
+                        ->withInput()
+                        ->with('payment-error', 'Thẻ tín dụng không hợp lệ, vui lòng kiểm tra lại.');
+                }
+            }
+
+            $carts = Session::get('cart')->items;
+            $totalAmount = $this->paymentCalculation($carts);
+
+            if ($request->voucher != NULL) {
+                $voucher = Voucher::where('code', $request->voucher)
+                    ->where('start_at', '<=', Carbon::now()->toDateString())
+                    ->where('end_at', '>=', Carbon::now()->toDateString())
+                    ->where('is_used', false)
+                    ->where('isDelete', false)
+                    ->first();
+                if ($voucher != NULL) {
+                    $totalAmount['totalPay'] -= $voucher->discount;   
+                } else {
+                    return redirect()
+                        ->back()
+                        ->withInput()
+                        ->with('payment-voucher-error', 'Voucher không hợp lệ hoặc đã hết hạn');
+                }
+            }
+
+            $bill = Bill::create([
+                'fullname' => $request->fullname,
+                'email' => $request->email,
+                'address' => $request->address,
+                'phone' => $request->phone,
+                'notes' => $request->notes ?? '',
+                'status' => $this->BILL_STATUS['IN_PROCESS'],
+                'totalPrice' => $totalAmount['totalPrice'],
+                'totalDiscount' => $totalAmount['totalDiscount'],
+                'shippingCost' => $totalAmount['totalShippingCost'],
+                'totalPay' => $totalAmount['totalPay'],
+                'voucher_id' => $voucher->id ?? null,
+                'method' => $request->paymentType,
+                'card_id' => $card->id ?? null,
+                'customer_id' => Auth::user()->id,
             ]);
 
-            $product = Product::find($key);
-            $product->sold += $value['qty'];
-            $product->save();
-        }
+            if (isset($voucher)) {
+                $voucher->is_used = true;
+                $voucher->save();
+            }
 
-        // send invoice to customer email
-        $customer = Customer::where('id', Auth::user()->id)->firstOrFail();
-        $customer->notify(new PaymentInvoiceNotification($carts, $bill, isset($voucher) ? $voucher : NULL));
-        
-        Session::forget('cart');
-        return redirect()->route('billDetail', $bill->id);
+            foreach($carts as $key => $value)
+            {
+                $bill_detail = BillDetail::create([
+                    'bill_id' => $bill->id,
+                    'product_id' => $key,
+                    'quantity' => $value['qty'],
+                    'unit_price' => $value['item']->price,
+                ]);
+
+                $product = Product::find($key);
+                $product->sold += $value['qty'];
+                $product->save();
+            }
+
+            // send invoice to customer email
+            $customer = Customer::where('id', Auth::user()->id)->firstOrFail();
+            $customer->notify(new PaymentInvoiceNotification($carts, $bill, isset($voucher) ? $voucher : NULL));
+            
+            Session::forget('cart');
+            return redirect()->route('billDetail', $bill->id);
+        }
+        catch(\Exception $error){
+            return view('error')
+                ->with('errorMessages', $error->getMessage())
+                ->with('returnUrl', url()->previous());
+        }
     }
 
     public function checkProductQuantity() {
-        $checkResult = $this->checkQuantity();
-        return $checkResult['status'] == 1 
-            ? response()->json(['isEnough' => true])
-            : response()->json([
-                'isEnough' => false, 
-                'product' => $checkResult['product'], 
-                'remaining' => $checkResult['availableQty'],
-                'missing' => $checkResult['missingQty'],
-            ]);
+        try {
+            $checkResult = $this->checkQuantity();
+            return $checkResult['status'] == 1 
+                ? response()->json(['isEnough' => true])
+                : response()->json([
+                    'isEnough' => false, 
+                    'product' => $checkResult['product'], 
+                    'remaining' => $checkResult['availableQty'],
+                    'missing' => $checkResult['missingQty'],
+                ]);
+        }
+        catch(\Exception $error){
+            return response()->json(['status' => 400, 'errorMessages' => $error->getMessage()], 400);
+        }
     }
 
     private function checkQuantity() {
